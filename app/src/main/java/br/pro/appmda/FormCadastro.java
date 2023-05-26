8cebc506218fcdae9ed6e851ee23fc86fb60b0e9
Mariana Bianchini Lima <53195959+mbl1997@users.noreply.github.com>
2023-05-25 21:30:35 -0300

    Initial commit

---
+++ app/src/main/java/br/pro/appmda/FormCadastro.java
@@ -0,0 +1,14 @@
+package br.pro.appmda;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
